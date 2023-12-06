07cc962e49c911bd25cc822857482a2c861e73b5
asaadefrei <asaad.mehidi@efrei.net>
2023-12-06 16:22:21 +0200

    up

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Pokemon;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class PokemonController extends AbstractController
 {
     #[Route('/pokemon', name: 'app_pokemon')]
-    public function index(): Response
-    {
+    public function index(ManagerRegistry $doctrine): Response
+    {    
+        $pokemons = $doctrine->getRepository(Pokemon::class)->findAll();
+
         return $this->render('pokemon/index.html.twig', [
             'controller_name' => 'PokemonController',
+            'pokemons' => $pokemons,
+
         ]);
     }
 }
