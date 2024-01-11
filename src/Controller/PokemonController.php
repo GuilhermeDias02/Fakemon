7b873ee862702d6adbae3ba3def4d7619c06071b
asaadefrei <asaad.mehidi@efrei.net>
2024-01-11 09:21:20 +0200

    up

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -7,21 +7,40 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\HttpClient;
+use Symfony\Component\HttpFoundation\Request;
 
 class PokemonController extends AbstractController
 {
-        #[Route('/pokemon', name: 'app_pokemon', methods: 'GET')]
+        #[Route('/pokemon/{pokemons?}', name: 'app_pokemon')]
 
-        public function index(HttpClientInterface $httpClient)
-        {   
-              //  dd(1);
-       
-                 $response = $this-> $httpClient ->request(
-                'GET',
-                'http://127.0.0.1:8000/api/pokema' 
-        );
-
-           // return $this->render('pokemon/index.html.twig');
+        public function index($pokemons,Request $request): Response
+        {
+          /*   
+          if ($request->attributes->has('pokemons')) {
+            $pokemons = json_decode(urldecode($request->attributes->get('pokemons')), true);
         }
-    }
+    
+        // $pokemons devrait maintenant contenir les données décodées
+        dd($pokemons);      
+*/
 
+              return $this->render('pokemon/index.html.twig');        
+          }
+
+
+          #[Route('/pokemonLoading', name: 'app_loading')]
+
+          public function chargement(): Response
+          {   
+  
+                return $this->render('LoadingApi/LoadingPokemon.html.twig');
+    }
+    #[Route('/l', name: 'app_l')]
+  
+    public function load(): Response
+    {   
+          return $this->render('LoadingApi/dd.html.twig');
+}
+
+  }
